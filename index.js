7f3ae56474c7b40bc413e36bb5352fc403cae6de
Your Name <you@example.com>
2021-01-12 20:41:53 +0330

    Add pino http logger

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const ArvanExporter = require('./arvanexporter')
 const { API_KEY, PORT, DOMAINS, MODE } = require('./config')
 const express = require('express')
+const pino = require('pino-http')()
 const app = express()
 
 const exporter = new ArvanExporter(API_KEY, DOMAINS, MODE)
 
+app.use(pino)
 app.use(async (req, res) => {
   res.send(await exporter.scrapeMetrics())
 })
