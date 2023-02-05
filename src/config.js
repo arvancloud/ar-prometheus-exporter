62bb4dc55fe21b35a71abcc73a6ea5cb8aa292f0
YaserAmini88 <yaser.amini67@gmail.com>
2023-02-05 14:33:03 +0330

    fix BASE_URL + add docker-compose sample file

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   API_KEY: env.API_KEY,
   DOMAINS: (typeof(env.DOMAINS) === 'string') ? [env.DOMAINS] : env.DOMAINS,
   UPDATE_INTERVAL: env.UPDATE_INTERVAL || 30000,
-  BASE_URL: env.BASE_URL || 'https://napi.arvancloud.com/cdn/4.0/',
+  BASE_URL: env.BASE_URL || 'https://napi.arvancloud.ir/cdn/4.0/',
   METRICS_PERIOD: env.METRICS_PERIOD || '3h',
   METRICS_PREFIX: env.METRICS_PREFIX || 'arvancloud_cdn_',
 }
