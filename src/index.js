be38209d17ab5800d685659ab9fb63d66973ec40
Arash Hatami <hatamiarash7@gmail.com>
2023-03-05 21:47:26 +0330

    Improve metrics output

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const ArvanExporter = require('./arvanexporter')
 const { API_KEY, PORT, DOMAINS, MODE } = require('./config')
-const express = require('express')
+const app = require('express')()
 const pino = require('pino-http')()
-const app = express()
+const promClient = require('prom-client')
 
 const exporter = new ArvanExporter(API_KEY, DOMAINS, MODE)
 
 app.use(pino)
 app.use(async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
   res.send(await exporter.scrapeMetrics())
 })
 
