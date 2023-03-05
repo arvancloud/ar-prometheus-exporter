dd0741080f93741073c42e9f230bceb75c8d3c0d
Arash Hatami <hatamiarash7@gmail.com>
2023-03-05 21:58:38 +0330

    Improve code

--- src/config.js
+++ src/config.js
@@ -1,13 +1,15 @@
 const dotenvParseVariables = require('dotenv-parse-variables');
 const process = require('process')
 
-env = dotenvParseVariables(process.env);
+// Parse environment variables using dotenv-parse-variables package
+const env = dotenvParseVariables(process.env);
 
+// Define configuration object with default values and values from environment variables
 const CONFIG = {
   MODE: env.MODE || 'PASSIVE',
   PORT: env.PORT || 9786,
   API_KEY: env.API_KEY,
-  DOMAINS: (typeof (env.DOMAINS) === 'string') ? [env.DOMAINS] : env.DOMAINS,
+  DOMAINS: Array.isArray(env.DOMAINS) ? env.DOMAINS : (typeof env.DOMAINS === 'string' ? [env.DOMAINS] : []),
   UPDATE_INTERVAL: env.UPDATE_INTERVAL || 30000,
   BASE_URL: env.BASE_URL || 'https://napi.arvancloud.ir/cdn/4.0/',
   METRICS_PERIOD: env.METRICS_PERIOD || '3h',
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-const ArvanExporter = require('./arvanexporter')
-const { API_KEY, PORT, DOMAINS, MODE } = require('./config')
-const app = require('express')()
-const pino = require('pino-http')()
-const promClient = require('prom-client')
+const ArvanExporter = require('./arvanexporter');
+const { API_KEY, PORT, DOMAINS, MODE } = require('./config');
+const app = require('express')();
+const pino = require('pino-http')();
+const promClient = require('prom-client');
 
-const exporter = new ArvanExporter(API_KEY, DOMAINS, MODE)
+const exporter = new ArvanExporter(API_KEY, DOMAINS, MODE);
+const contentType = promClient.register.contentType;
 
-app.use(pino)
+app.use(pino);
+
 app.use(async (req, res) => {
-  res.set('Content-Type', promClient.register.contentType);
-  res.send(await exporter.scrapeMetrics())
-})
+  try {
+    res.set('Content-Type', contentType);
+    const metrics = await exporter.scrapeMetrics();
+    res.send(metrics);
+  } catch (err) {
+    console.error(`Error scraping metrics: ${err}`);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(PORT, () => {
-  console.log(`Exporter listening at port ${PORT}`)
-})
+  console.log(`Exporter listening at port ${PORT}`);
+});
